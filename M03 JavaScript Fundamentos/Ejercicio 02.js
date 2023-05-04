2d31f40e46dd5dd423015357cd413a3745222d20
alejandrorodriguez <alejandrorodriguezjimenez17@outlook.com>
2023-05-04 15:13:08 -0500

    Module 3 exercises solved

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var z = x + y;
+   return z;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var z = x - y;
+   return z;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var z = x / y;
+   return z;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var z = x * y;
+   return z;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var z = x % y;
+   return z;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
