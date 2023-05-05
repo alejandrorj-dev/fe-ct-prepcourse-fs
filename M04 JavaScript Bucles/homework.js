bdcb48871b2e2c4cfd22274574cfc80ed41ab027
alejandrorodriguez <alejandrorodriguezjimenez17@outlook.com>
2023-05-05 10:41:31 -0500

    Module 4 exercises solved

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -178,11 +178,18 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
-   if (num % num === 0 && num % 1 === 0 && num > 1) {
-      return true;
-   } else {
+   if (num < 2) {
       return false;
    }
+
+   for (var i = 2; i < num; i++) {
+      if (num % i === 0 || num % num != 0 || num % 1 != 0) {
+         return false;
+      }
+   }
+
+   return true;
+
 }
 
 function esVerdadero(valor) {
