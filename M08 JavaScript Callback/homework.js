5f63230af81ee756c70c690e688e23aca10cb5a4
alejandrorodriguez <alejandrorodriguezjimenez17@outlook.com>
2023-05-11 21:38:28 -0500

    Progress of exercises of module 8 (not tested)

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,16 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   var arrNombre = nombre.split("");
+   String.upperCase(arrNombre[0]);
+   return arrNombre.join("");
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,6 +22,11 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   var realizarOperacion = function (num1, num2) {
+      return cb(num1, num2);
+   }
+
+   realizarOperacion(num1, num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -26,6 +35,14 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   var sumarArreglo = function (arrNumbers) {
+      var suma = 0;
+      for (var i = 0; i < arrNumbers.length; i++) {
+         suma = suma + arrNumbers[i];
+      }
+      cb(suma);
+   };
+   sumarArreglo(arrayOfNumbers);
 }
 
 function forEach(array, cb) {
@@ -33,6 +50,12 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   var ejecutarCallbackPorElemento = function (arr) {
+      for (var i = 0; i < arr.length; i++) {
+          cb(arr[i]);
+      }
+   };
+   ejecutarCallbackPorElemento(array);
 }
 
 function map(array, cb) {
@@ -40,6 +63,7 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   
 }
 
 function filter(arrayOfStrings) {
