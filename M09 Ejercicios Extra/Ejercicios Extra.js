b1de87ba927ea2736887b6f3408aa26b9f0ce152
alejandrorodriguez <alejandrorodriguezjimenez17@outlook.com>
2023-05-17 17:43:45 -0500

    Progress of exercise solving of module 9

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,7 +6,15 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   return Object.entries(objeto);
+
 }
+var miTelefono = {
+   marca: "Samsung",
+   modelo: "A21S",
+   camaras: 2
+}
+console.log(deObjetoAarray(miTelefono));
 
 function numberOfCharacters(string) {
    // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
@@ -14,7 +22,22 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   var StringObject = {};
+   var stringArr = string.split("");
+   var counter = 0;
+
+   for (var i = 0; i < stringArr.length; i++) {
+      if (stringArr[i] != stringArr[i+1]) {
+         StringObject[stringArr[i]] = 1;
+      } else if (stringArr[i] === stringArr[i+1]) {
+         counter = counter + 1;
+         StringObject[stringArr[i]] = counter;
+      }
+   };
+
+   return StringObject;
 }
+console.log(numberOfCharacters("esternocleidomastoideo"));
 
 function capToFront(string) {
    // Recibes un string con algunas letras en mayúscula y otras en minúscula.
@@ -22,20 +45,79 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   var stringArray = string.split("");
+   var newString = [];
+
+   for (var i = 0; i < stringArray.length; i++) {
+      if (stringArray[i] === stringArray[i].toUpperCase()) {
+         newString.unshift(stringArray[i]);
+      } else if (stringArray[i] === stringArray[i].toLowerCase()) {
+         newString[i].push(stringArray[i]);
+      }
+   }
+
+   return newString.join(" ");
 }
+console.log("soyHENRY");
 
 function asAmirror(frase) {
    // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   var fraseEnArreglo = frase.split("");
+   var resultado = [];
+   var palabra = "";
+
+   for (var i = 0; i < fraseEnArreglo.length; i++) {
+      if (fraseEnArreglo[i] !== " ") {
+         palabra = palabra + fraseEnArreglo[i];
+      } else {
+         resultado.push(palabra);
+         palabra = "";
+      }
+   }
+
+   resultado.push(palabra);
+
+   var resultadoMap = resultado.map( (element) => {
+      return element.split("").reverse().join("");
+   });
+
+   return resultadoMap.join(" ");
+   
 }
+console.log(asAmirror("Me gusta beber agua"));
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   var numeroEnArreglo = Array.from(String(numero), num => Number(num));
+   var otrosNumeros = [];
+   var esCapicua = true;
+   var c = 0;
+
+   while (c < numeroEnArreglo.length) {
+      otrosNumeros.unshift(numeroEnArreglo[c]);
+      c++;
+   }
+
+   for (var i = numeroEnArreglo.length; i >= 0; i--) {
+      if (numeroEnArreglo[i] !== otrosNumeros[i]) {
+         esCapicua = false;
+         break;
+      }
+   }
+
+   if (esCapicua) {
+      return "Es capicua";
+   } else {
+      return "No es capicua";
+   }
+   
 }
+console.log(capicua(3003));
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
